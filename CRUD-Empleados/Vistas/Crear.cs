bcf95291525aa80c21971a37b9954e13ab893074
accusync70 <lizanderangel@gmail.com>
2017-09-10 21:59:52 -0400

    Arreglo vistra crear

--- CRUD-Empleados/Vistas/Crear.cs
+++ CRUD-Empleados/Vistas/Crear.cs
@@ -14,10 +14,6 @@ namespace CRUD_Empleados
 {
     public partial class Crear : Form
     {
-        //Conexion a base de datos
-        SqlConnection cn = new SqlConnection(
-        ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
-
 
         public Crear()
         {
@@ -34,31 +30,6 @@ namespace CRUD_Empleados
         {
             var Empleados = new Clases.Empleados(txtnombre.Text, txtapellido.Text, txtcedula.Text, txtdireccion.Text);
             Empleados.registrar();
-
-
-           /* string query = "INSERT INTO EMPLEADOS (NOMBRE, APELLIDO, CEDULA, DIRECCION)";
-            query += " VALUES (@NOMBRE, @APELLIDO, @CEDULA, @DIRECCION)";
-
-            try
-            {
-                cn.Open();
-                SqlCommand myCommand = new SqlCommand(query, cn);
-                myCommand.Parameters.AddWithValue("@NOMBRE", txtnombre.Text);
-                myCommand.Parameters.AddWithValue("@APELLIDO", txtapellido.Text);
-                myCommand.Parameters.AddWithValue("@DIRECCION", txtdireccion.Text);
-                myCommand.Parameters.AddWithValue("@CEDULA", txtcedula.Text);
-                myCommand.ExecuteNonQuery();
-                cn.Close();
-                MessageBox.Show("Datos insertados correctamente");
-            }
-
-            catch (SqlException p)
-            {
-  
-                MessageBox.Show(p.Message);
-            }*/
-                
-            
         }
 
         private void btncancelar_Click(object sender, EventArgs e)
